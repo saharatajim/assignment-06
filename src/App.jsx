9114880a90f59e94e781e2faa835d6e4a5340712
saharatajim <sahara.tajim@gmail.com>
2026-04-01 02:43:28 +0600

    toggling produc&cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css'
 import NavBar from './components/NavBar/NavBar';
 import Banner from './components/Banner/Banner';
@@ -7,13 +7,27 @@ import Stats from './components/Stats/Stats.jsx';
 import Workflow from './components/Workflow/Workflow';
 import StepsSection from './components/StepsSection/StepsSection';
 import PricingSection from './components/PricingSection/PricingSection';
+
 const App = () => {
+ const [selectedTab,setSelectedTab]=useState("products")
 
   return (
    <>
    <NavBar></NavBar>
    <Banner></Banner>
    <Stats></Stats>
+   <section className='text-center container mx-auto my-[60px]'>
+    <div className='space-y-5'> 
+      <h1 className='font-extrabold text-[48px]'>Premium Digital Tools</h1>
+      <p>Choose from our curated collection of premium digital products designed <br />
+         to boost your productivity and creativity.</p>
+      <div>
+        <button onClick={()=>setSelectedTab("products")} className={`btn ${selectedTab==="products" &&'gradient-bg text-white' } rounded-l-full `}>Products</button>
+        <button onClick={()=>setSelectedTab("cart")} className={`btn bg-base-100 rounded-r-full ${selectedTab==="cart"&& 'gradient-bg text-white'}`}>Cart (0)</button>
+      </div>
+    
+    </div>
+   </section>
    <StepsSection></StepsSection>
   <PricingSection></PricingSection>
    <Workflow></Workflow>
