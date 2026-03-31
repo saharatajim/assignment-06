5ece8ed46467e6f822bd09d4d01f26cc00e3bc54
saharatajim <sahara.tajim@gmail.com>
2026-04-01 02:05:03 +0600

    all styles and structure build done

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css'
+import NavBar from './components/NavBar/NavBar';
+import Banner from './components/Banner/Banner';
+import Footer from './components/footer/Footer';
+import Stats from './components/Stats/Stats.jsx';
+import Workflow from './components/Workflow/Workflow';
+import StepsSection from './components/StepsSection/StepsSection';
+import PricingSection from './components/PricingSection/PricingSection';
+const App = () => {
+
+  return (
+   <>
+   <NavBar></NavBar>
+   <Banner></Banner>
+   <Stats></Stats>
+   <StepsSection></StepsSection>
+  <PricingSection></PricingSection>
+   <Workflow></Workflow>
+   <Footer></Footer>
+   </>
+  );
+};
+
+export default App;
